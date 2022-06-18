3c98efbbf636e7397b09832270e04bdf228a8136
Drake K. Owen <owendk@AutomatedResults.Biz>
2022-06-18 14:08:16 -0400

    fixed main menu glitch

--- JS/mainMenu.js
+++ JS/mainMenu.js
@@ -5,5 +5,5 @@ const startGame = document.getElementById('start-game');
 
 startGame.addEventListener('click', event => {
     console.log('I was clicked')
-    window.location.href = './HTML/levelOne.html'
+    window.location = './HTML/levelOne.html'
 })
