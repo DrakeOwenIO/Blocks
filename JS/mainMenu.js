70bea33f6338d918942f2c39385d237a869ffc24
Drake K. Owen <owendk@AutomatedResults.Biz>
2022-06-18 12:10:39 -0400

    Basic main menu done

--- JS/mainMenu.js
+++ JS/mainMenu.js
@@ -1,2 +1,9 @@
 console.log("I am linked!")
 
+const startGame = document.getElementById('start-game');
+
+
+startGame.addEventListener('click', event => {
+    console.log('I was clicked')
+    window.location.href = './HTML/levelOne.html'
+})
