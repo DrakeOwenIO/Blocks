a6cd6fb0c3d43ddc64d46f6264fccc974ddaefa9
Drake K. Owen <owendk@AutomatedResults.Biz>
2022-06-18 14:18:54 -0400

    Re-worked main menu code slightly

--- JS/mainMenu.js
+++ JS/mainMenu.js
@@ -6,4 +6,5 @@ const startGame = document.getElementById('start-game');
 startGame.addEventListener('click', event => {
     console.log('hello click')
     window.open('./HTML/levelOne.html')
+    window.close();
 })
