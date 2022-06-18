6e67d1fc7181baaa77d02c24d80277370fd8f3d8
Drake K. Owen <owendk@AutomatedResults.Biz>
2022-06-18 15:02:56 -0400

    Added gametip telling player how to move

--- JS/mainDriver.js
+++ JS/mainDriver.js
@@ -1,29 +1,38 @@
+// Defining the play area
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
+var canvasWidth = 0;
+var canvasHeight = 0;
 
+// Defining Player Properties
 var playerX = 0;
 var playerY = 0;
 var playerSize = 100;
 var playerSpeed = 5;
 
-var canvasWidth = 0;
-var canvasHeight = 0;
-
+// Recording keypresses
 var keys = [];
 
-var collision = false;
+// Collision Dectection Variables
+var touchingLeftWall = false;
+var touchingRightWall = false;
+var touchingTopWall = false;
+var touchingBottomWall = false;
 
 // Key pressed check
 window.addEventListener("keydown", function(e){
     keys[e.keyCode] = true;
 })
 
+// Key released check
 window.addEventListener("keyup", function(e){
     keys[e.keyCode] = false;
 })
 
-// initialization
+// Initialization
 function init(){
+    touchingLeftWall = false;
+
     canvasWidth = 800;
     canvasHeight = 600;
 
@@ -34,38 +43,33 @@ function init(){
     playerY = canvasHeight/2;
 }
 
-// gameloop
+// Gameloop
 function loop(){
     update();
     render();
 }
 
-// Update the game window
+// Where all the changes to the screen happens
 function update(){
 
 // MOVEMENT KEYS
-    // Move Left
+    // Move Left | A
     if(keys[65] == true) {
-        playerX = playerX - playerSpeed;
-
-        console.log('PlayerX: ' + playerX);
-        console.log('PlayerY: ' + playerY);
+        playerX = playerX - playerSpeed;   
     }
 
-    // Move Up
+    // Move Up | W
     if(keys[87] == true) {
         playerY = playerY - playerSpeed;
 
-        console.log('PlayerX: ' + playerX);
-        console.log('PlayerY: ' + playerY);
     }
 
-    // Move Down
+    // Move Down | S
     if(keys[83] == true) {
         playerY = playerY + playerSpeed;
     }
 
-    //Move Right
+    //Move Right | D
     if(keys[68] == true) {
         playerX = playerX + playerSpeed;
     }
@@ -74,12 +78,14 @@ function update(){
 //Game Boundary Collision
     // Left Wall
     if(playerX == canvasWidth - canvasWidth) {
-        
+        touchingLeftWall = true;
+        playerX = 0;
     }
 
+    console.log(touchingLeftWall);
 }
 
-// render drawings
+// Render stuff on the screen
 function render(){
     context.clearRect(0, 0, canvasWidth, canvasHeight);
     context.fillStyle = "#FF00C1";
@@ -87,6 +93,8 @@ function render(){
 
 }
 
+// Running the game at 60fps
 window.setInterval(loop, 1000/60);
 
+// Calling the initialization
 init();
