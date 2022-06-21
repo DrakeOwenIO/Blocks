f2ae5eae64e8f29380f195ba49a89c92264ee011
Drake K. Owen <owendk@AutomatedResults.Biz>
2022-06-20 22:08:58 -0400

    Added score counter

--- JS/mainDriver.js
+++ JS/mainDriver.js
@@ -1,23 +1,23 @@
-import Obstacle from "./obstacleClass.js";
 import Player from "./playerClass.js"
 import {spawn} from "./enemySpawn.js";
 
-// Defining the play area
+// Defining the play area and score
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
 var canvasWidth = 0;
 var canvasHeight = 0;
+var score = 0;
+var scoreTimer = 0;
+var scoreCounter = document.getElementById('score');
 
 // Recording keypresses
 var keys = [];
 
 // Create player
-const mainPlayer = new Player(0, 0, 100, 5);
+const mainPlayer = new Player(0, 0, 90, 5);
 
-// Temp Rectangle
-var rect = new Obstacle(670, 15, 200, 100, 5)
+// Enemy Spawning Variables
 var spawnTimer = 0;
-var newEnemy = null;
 var enemyArray = {};
 var enemyCount = 4;
 
@@ -56,6 +56,9 @@ function init(){
     canvas.setAttribute("width", canvasWidth)
     canvas.setAttribute("height", canvasHeight)
 
+    // Start score counter
+    scoreCounter.innerText = 'Score: ' + score;
+
     // Player Starting Position
     mainPlayer.posX = canvasWidth / 2 - (mainPlayer.size / 2);
     mainPlayer.posY = canvasHeight / 2 - (mainPlayer.size / 2);
@@ -73,16 +76,7 @@ function update(){
     // Let the player move
     mainPlayer.movement(keys, mainPlayer, canvasWidth, canvasHeight)
 
-    // Collision Detection
-    // ADD COLLISION DETECTION TO PLAYER CLASS?
-    if(mainPlayer.posX + mainPlayer.size >= rect.posX && 
-        mainPlayer.posX <= rect.posX + rect.wid && 
-        mainPlayer.posY + mainPlayer.size >= rect.posY &&
-        mainPlayer.posY <= rect.posY + rect.len)
-    {
-        gameOver();
-    }
-
+    // Spawn new enemies
     if(spawnTimer === 100){
         console.log('calling spawn');
         for (let i = 0; i < enemyCount; i++)
@@ -93,21 +87,27 @@ function update(){
         spawnTimer = 0;
     }
 
-    // rect.move(rect);
+    // Update Score
+    if(scoreTimer === 60)
+    {
+        score += 1;
+        scoreCounter = document.getElementById('score');
+        scoreCounter.innerHTML = 'Score: ' + score;
+
+        scoreTimer = 0;
+    }
+
+    // Increment Timers
     spawnTimer += 1;
+    scoreTimer += 1;
 }
 
 // Render stuff to screen
 function render(){
     // Call Player Renderer
     mainPlayer.render(context, mainPlayer, canvasWidth, canvasHeight);
-    
-    // Call Obstacle Renderer
-    rect.render(context, rect);
 
-    // newEnemy.render(context, newEnemy);
-    // newEnemy.move(newEnemy);
-
+    // Render Enemies
     for(let i = 0; i < enemyCount; i++)
     {
         enemyArray[i].render(context, enemyArray[i]);
